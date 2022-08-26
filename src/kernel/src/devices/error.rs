d9ef8d683ab72135601266f1065ec7310c15a0aa
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-25 18:22:54 -0700

    Add derives for DeviceError

--- src/kernel/src/devices/error.rs
+++ src/kernel/src/devices/error.rs
@@ -1,4 +1,4 @@
 /// Representation of an error as the result of an I/O operation on a device. Provided through all
 /// I/O routines of devices (since they all return a `Result`).
-#[derive(Debug, Clone)]
+#[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct DeviceError;
