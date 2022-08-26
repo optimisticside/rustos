59df667da93c476727c4bd225fe41fb2e6f40f5c
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-25 17:19:36 -0700

    Remove unecessary I-node-related code

---
+++ src/kernel/src/filesys/ext2/inode.rs
@@ -0,0 +1,43 @@
+/// An I-node on the EXT-2 file system is, though of the same name, not the same as an I-node in
+/// the virtual file system. This is why all BSD operating systems call i-nodes in the VFS v-nodes
+/// instead.
+#[repr(C, packed)]
+pub struct Inode {
+    /// Type and permissions.
+    mode: u16,
+    /// User ID.
+    uid: u16,
+    /// Lower 32 bits of the size in bytes.
+    size: u32,
+    /// Last access time.
+    access_time: u32,
+    /// Creation time.
+    ctime: u32,
+    /// Last modification time.
+    mtime: u32,
+    /// Group ID.
+    gid: u16,
+    /// Number of hard links (directory entries) to this inode. Data blocks are unallocated when
+    /// this reaches 0.
+    link_count: u16,
+    /// Number of disk sectors in use by the inode, not including the i-node itself or any
+    /// directory entries.
+    block_count: u16,
+    /// Flags.
+    flags: u32,
+    reserved: u32,
+    /// Disk block pointers.
+    /// Entires 1-11: Direct block pointers.
+    /// Entry 12: Singly indirect block pointer.
+    /// Entry 13: Doubly indirect block pointer.
+    /// Entry 14: Triply indirect block pointer.
+    blocks: [u32; 15],
+    /// Generation number (mostly used for NFS).
+    version: u32,
+    file_acl: u32,
+    dir_acl: u32,
+    /// Block address of fragment.
+    frag_addr:u32,
+    frag_num: u32,
+    reserved1: [u16; 5],
+}
--- src/kernel/src/filesys/inode.rs
+++ src/kernel/src/filesys/inode.rs
@@ -1,20 +1,3 @@
-/// Types of I-nodes.
-enum InodeKind {
-    Null,
-    Directory,
-    Block,
-    Character,
-    Link,
-    Socket,
-    Fifo,
-    Bad,
-}
-
 /// An I-node is the focus of file activity on UNIX system. There is one allocated for every active
 /// file, directory, mounted-file, and the file-system's root.
-pub struct Inode {
-    /// The type of the I-node.
-    kind: InodeKind,
-    /// Specific fields that are specific to the type of the I-node.
-    
-}
+pub struct Inode;
