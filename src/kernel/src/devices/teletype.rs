ec96c15c2707e006589f2c273738b1c8049f2273
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-28 16:11:44 -0700

    Add more terminal flags

--- src/kernel/src/devices/teletype.rs
+++ src/kernel/src/devices/teletype.rs
@@ -41,11 +41,82 @@ bitflags::bitflags! {
         const MAP_NL_CRNL = 1 << 1;
         /// Map carriage-return characters to new-line characters.
         const MAP_CR_NL = 1 << 2;
+        /// No carriage return at column 0.
+        const NO_CR_COL0 = 1 << 3;
+        /// New-line characters perform the function of carriage-return characters.
+        const NL_AS_CR = 1 << 4;
+        /// Fill characters are DELETEs.
+        const FILL_DELETE = 1 << 5;
+        /// Use fill characters for delay.
+        const FILL_DELAY = 1 << 6;
+    }
+}
+
+bitflags::bitflags! {
+    pub struct TeletypeControlFlags: usize {
+        const HANG_UP = 1;
+        const BAUD_50 = 1;
+        const BAUD_75 = 2;
+        const BAUD_110 = 3;
+        const BAUD_134 = 4;
+        const BAUD_150 = 5;
+        const BAUD_200 = 6;
+        const BAUD_300 = 7;
+        const BAUD_600 = 8;
+        const BAUD_1200 = 9;
+        const BAUD_1800 = 10;
+        const BAUD_2400 = 11;
+        const BAUD_2800 = 12;
+        const BAUD_9600 = 13;
+        const BAUD_19200 = 14;
+        const BAUD_38400 = 15;
+    }
+}
+
+bitflags::bitflags! {
+    pub struct TeletypeLocalFlags: usize {
+        /// Enable echo.
+        const LOCAL = 1 << 0;
+        /// Echo erase-character as error-correcting backspace.
+        const ECHO_ERASE = 1 << 1;
+        /// Echo kill-characters.
+        const ECHO_KILL = 1 << 2;
+        /// Echo new-line characters.
+        const ECHO_NL = 1 << 3;
+        /// Canonical input (erase and kill processing).
+        const CANONICAL = 1 << 4;
+        /// Enable extended input-processing.
+        const EXTENDED_INPUT = 1 << 5;
+        /// Enable signals.
+        const SIGNALS = 1 << 6;
+        /// Disable flush after interrupt or quit signal.
+        const NO_FLUSH = 1 << 7;
+        /// Send SIGTTOU for background output.
+        const OUTPUT_STOP = 1 << 8;
     }
 }
 
 /// Control characters.
 pub struct TeletypeControlChars {
+    interrupt: u8,
+    quit: u8,
+    erase: u8,
+    kill: u8,
+    end_of_file: u8,
+    /// Time-out value (measured in 1/10ths of a second).
+    timeout: u8,
+    /// Minimum number of bytes that can be read at once.
+    min_bytes: u8,
+    swtc: u8,
+    start: u8,
+    stop: u8,
+    suspend: u8,
+    end_of_line: u8,
+    reprint_line: u8,
+    discard: u8,
+    word_erase: u8,
+    literal_next: u8,
+    end_of_line2: u8,
 }
 
 /// Terminal control structure (also known as termios).
