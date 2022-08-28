5b90cba708f05376202ecb741cf3fc068f5850fe
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-27 17:03:13 -0700

    Add fields for Teletype struct

--- src/kernel/src/devices/teletype.rs
+++ src/kernel/src/devices/teletype.rs
@@ -1,3 +1,20 @@
+use alloc::collections::vec_deque::VecDeque;
+use crate::devices::CharDeviceSwitch;
+
+/// Teletypes abstract over character devices, just like a buffered-device would abstract over a
+/// block device.
 pub struct Teletype {
+    /// Internal character device.
+    device: dyn CharDeviceSwitch,
+    /// Output queue.
+    output_queue: VecDeque<u8>,
+    /// Canonical queue. This is when terminal input is processed in lines terminated by \n. No
+    /// input can be read until the entire line has been read by the user.
+    canonical_queue: VecDeque<u8>,
+    /// Raw input quque.
+    input_queue: VecDeque<u8>,
+}
+
+impl CharDeviceSwitch for Teletype {
     
 }
