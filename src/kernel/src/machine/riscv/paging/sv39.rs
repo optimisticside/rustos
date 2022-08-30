3979f26496b36f00e3570346f43dc454bab66ded
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-29 18:28:07 -0700

    Implement SV39 mapping

--- src/kernel/src/machine/riscv/paging/sv39.rs
+++ src/kernel/src/machine/riscv/paging/sv39.rs
@@ -38,7 +38,17 @@ pub struct PageTableEntry(usize);
 impl PageTableEntry {
     /// Retrieve the 0th PPN.
     pub fn ppn0(&self) -> usize {
-        
+        (self.0 >> 12) & 0x1FF
+    }
+
+    /// Retrieve the 1th PPN.
+    pub fn ppn1(&self) -> usize {
+        (self.0 >> 21) & 0x1FF
+    }
+
+    /// Retrieve the 2nd PPN.
+    pub fn ppn2(&self) -> usize {
+        (self.0 >> 30) & 0x3FF_FFFF
     }
 }
 
@@ -54,8 +64,41 @@ bitflags::bitflags! {
         const SUP_WRITE = 5 << 3;
         const SUP_READ = 6 << 3;
         /// The entry has been read from.
-        const REFERENCED = 1 << 4;
+        const ACCESSED = 1 << 4;
         /// The entry has been written to.
         const DIRTY = 1 << 5;
+    }
+}
+
+/// Page-table structure. The only thing that it holds is an array of entries. (I question why I
+/// even made this a structure instead of just an array).
+#[repr(C, packed)]
+pub struct PageTable {
+    pub entries: [PageTableEntry; 512],
+}
+
+impl PageTable {
+    /// Maps a physical address to a virtual address. Called internally by PageTable::map.
+    pub fn map_address(
+        &mut self,
+        virt_addr: usize,
+        phys_addr: usize,
+        flags: PageTableEntryFlags,
+        level: usize
+    ) {
+        let virt_addr = VirtAddr(virt_addr);
+        let mut entry = &mut self.entries[virt_addr.vpn2()];
+
+        for level in (level..2).rev() {
+            if entry & PageTableEntryFLags::VISIBLE as usize != 0 {
+                let page = PageTable::new();
+                *entry = PageTableEntry::new(page as usize, PageTableEntryFlags::VISIBLE);
+            }
+
+            let new_entry = entry.phys_addr().0 as *mut PageTableEntry;
+            *entry = unsafe { new_entry.add(virt_addr.index(level)).as_mut().unwrap() };
+        }
+
+        *entry = PageTableEntry::new(phys_addr, (flags | PageTableEntryFlags::VISIBLE).bits());
     }
 }
