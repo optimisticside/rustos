10a7a57029228c71b311a5f54b005d212892711b
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-09-05 12:14:05 -0700

    Update generic Error enum

--- src/kernel/src/error.rs
+++ src/kernel/src/error.rs
@@ -1,20 +1,12 @@
+use crate::devices::DeviceError;
+use crate::filesys::FileSystemError;
+
 /// Representation of an error as the result of any operation done on the system. These includes
 /// device errors, file-system errors, and other types of errors.
 #[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error { 
-    /// File-system errors.
-    NotSupported,
-    EntryExists,
-    EntryNotFound,
-    Busy,
-    NotDirectory,
-    IsPipe,
-    IsDirectory,
-    Interrupted,
-    TooSmall,
-    InvalidPath,
-    NotSocket,
-    ConnectionRefused,
-    NotConnected,
-    WouldBlock,
+    /// File-system error.
+    FileSystem(FileSystemError),
+    /// Device error.
+    DeviceError(DeviceError),
 }
