baa0974fcd3d30f44b3dd62cfa9ab0a4c2bb5ca7
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-09-26 19:53:28 -0700

    Set up syncronization system

--- src/kernel/src/sync/mutex.rs
+++
@@ -1,6 +0,0 @@
-/// A mutex is anything that can be locked and unlocked. This can be implemented by a sleep lock,
-/// spin lock, etc.
-pub trait Mutex<T> {
-    fn lock(&mut self) -> MutexGuard<T>;
-    fn unlock(&
-}
