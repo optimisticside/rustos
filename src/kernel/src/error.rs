f4ca5da62d775c2d70a58692adf48ce98caf5daf
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-12-03 16:59:49 -0800

    Fix typo in import

--- src/kernel/src/error.rs
+++ src/kernel/src/error.rs
@@ -1,10 +1,10 @@
-use crate::devices::DeviceError;
+use crate::device::DeviceError;
 use crate::filesys::FileSystemError;
 
 /// Representation of an error as the result of any operation done on the system. These includes
 /// device errors, file-system errors, and other types of errors.
 #[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub enum Error { 
+pub enum Error {
     /// File-system error.
     FileSystem(FileSystemError),
     /// Device error.
